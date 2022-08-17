d6b5e9dc0749da9a1e875b8ba5a3fcd8379c6835
danilobatson <djbatson19@gmail.com>
2022-08-16 20:00:16 -0700

    setting up store

--- music/src/main.js
+++ music/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { store } from './stores/counterVuex.js';
 
 import App from './App.vue';
 import router from './router';
@@ -10,6 +11,7 @@ import './assets/main.css';
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(store);
 app.use(router);
 
 app.mount('#app');
