920d2d6fb7dfff83ac4b8bc788eee2a9ddd2a50d
danilobatson <djbatson19@gmail.com>
2022-08-17 23:59:34 -0700

    router redirect

--- music/src/router/index.js
+++ music/src/router/index.js
@@ -1,15 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import About from '@/views/About.vue';
+import Manage from '@/views/Manage.vue';
 
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/about', name: 'about', component: About },
+  { path: '/manage-music', name: 'manage', component: Manage },
+  { path: '/manage', redirect: { name: 'manage' } },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500',
 });
 
 export default router;
