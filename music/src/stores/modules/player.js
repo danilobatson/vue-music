0992407184c18d4f1da25794220e458a501f7256
danilobatson <djbatson19@gmail.com>
2022-08-18 19:32:45 -0700

    plays song

--- music/src/stores/modules/player.js
+++ music/src/stores/modules/player.js
@@ -1,5 +1,8 @@
+import { Howl } from 'howler';
+
 const state = () => ({
   currentSong: {},
+  sound: {},
 });
 
 // getters
@@ -9,6 +12,7 @@ const getters = {};
 const actions = {
   async newSong({ commit }, song) {
     commit('newSong', song);
+    commit('playSong');
   },
 };
 
@@ -16,6 +20,14 @@ const actions = {
 const mutations = {
   newSong(state, song) {
     state.currentSong = song;
+
+    state.sound = new Howl({
+      src: [song.url],
+      html5: true, // Force to HTML5 so that the audio can stream in (best for large files).
+    });
+  },
+  playSong(state) {
+    state.sound.play();
   },
 };
 
